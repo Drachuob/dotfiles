e2a7b066b993e3ac6a04d1d24af3ca54efda1e49
Cyrvan Bouchard <cyrvan.bouchard@gmail.com>
2018-04-30 13:58:23 +0200

    Update installation script.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 - vim
 - pandoc
+- texlive
 - mupdf
+- git
 
 ## INSTALLATION
 
--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
-echo "INSTALL"
+#!/bin/bash
 
-# Install vimrc
+dependencies=(
+    'vim'
+    'pandoc'
+    'texlive'
+    'mupdf'
+    'git'
+)
+
+for dependencie in ${dependencies[@]}; do
+    sudo dnf install $dependencie
+done
+
+# Install vim configuration file
 cp vim/.vimrc $HOME
 
 # Install some document templates
@@ -11,3 +23,5 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+echo "Installation done."
