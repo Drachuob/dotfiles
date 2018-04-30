5cfa241f96b11d01c469bf310300b12b968d6e37
Cyrvan Bouchard <cyrvan.bouchard@gmail.com>
2018-04-30 13:48:34 +0200

    Add git aliases.

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ cp vim/.vimrc $HOME
 
 # Install some document templates
 cp templates/presentation.md $HOME/Templates/
+
+# Git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
